0c02f41cbdb1ddf99c202613c1b81026d4760ade
dindias <indias.corral.daniel@alumnat.copernic.cat>
2020-10-01 14:11:30 +0200

    dic-funcion daniel indias -2

--- OL-Grupal.php
+++ OL-Grupal.php
@@ -15,7 +15,7 @@ and open the template in the editor.
             echo 'hola';
         }
         function funcion3() {
-            echo 'hola';
+            echo 'VCJ-hola';
         }
         function funcion2() {
             echo 'dic-hola';
